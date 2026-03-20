6c6b80ea5df3234c29cd055c61d1003a1f4f9cd7
Phumlani Arendse <aphumlani.dev@gmail.com>
2026-03-20 20:50:20 +0200

    fixed lint error

--- generate-env.js
+++ generate-env.js
@@ -12,7 +12,7 @@ if (missing.length) {
 const content = `export const environment = {
   production: true,
   firebase: {
-    tmdbApiKey: '${process.env.TMDB_API_KEY}'
+    tmdbApiKey: '${process.env.TMDB_API_KEY}',
     tmdbBaseUrl: 'https://api.themoviedb.org/3',
     tmdbImageBaseUrl: 'https://image.tmdb.org/t/p/',
   }
